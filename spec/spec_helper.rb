a7136d0207557adec404c41c83bfbd10e407031a
Steven Barragán <me@steven.mx>
2015-12-11 11:34:38 -0800

    Fix codeclimate test coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'ostruct'
 require 'pry-byebug'
-
 require 'julia'
-
-CodeClimate::TestReporter.start
