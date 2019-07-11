a525ea9b399ea16d029bb53b12200145599db5fc
Stas SUȘCOV <stas@nerd.ro>
2019-07-11 13:36:08 +0100

    Remove old/unused gems. Added coverage support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+
+SimpleCov.start do
+  add_group 'Lib', 'lib'
+  add_group 'Tests', 'spec'
+end
+SimpleCov.minimum_coverage 100
 
 require 'ostruct'
-require 'pry'
 require 'julia'
